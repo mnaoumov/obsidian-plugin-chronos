787e7b2e31cb37fab79e7c598345da3957a20656
Claire Froelich <claire.froelich@gmail.com>
2025-01-10 17:33:24 -0500

    Fix middle click cursor icon behavior for windows?

--- main.ts
+++ main.ts
@@ -115,6 +115,7 @@ export default class ChronosPlugin extends Plugin {
       timeline.on("mouseDown", (event) => {
         const itemId = event.item;
         if (itemId) {
+          event.preventDefault();
           const item = timeline.items?.find((i) => i.id === itemId);
 
           const openInNewLeaf =
